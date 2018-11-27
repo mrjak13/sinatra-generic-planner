69f7c03e1100794f0fd8bde95df2242db7fa87d1
Justin Kenney <justinkenney13@gmail.com>
2018-11-27 23:07:56 +0000

    add sessions and secure password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "sinatra_events_calander_app"
+    register Sinatra::Flash
   end
 
   get "/" do
@@ -13,6 +16,18 @@ class ApplicationController < Sinatra::Base
 
   get '/signup' do
     erb :signup
+  end
+
+  post '/signup' do
+    if params.any?{|k,v| v == ""}
+      redirect to '/signup'
+    elsif User.find_by(email: params[:email])
+      redirect to '/login'
+    else
+      @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+      @user.save
+    end
+
   end
 
   get '/login' do
