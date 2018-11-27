cd3c65052511dc3bee1f1cb6c27b0be4808f7e7c
Justin Kenney <justinkenney13@gmail.com>
2018-11-27 19:33:04 +0000

    update for has_belongs_to_many association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,4 @@
 class Event < ActiveRecord::Base
   belongs_to :user
-  has_many :events_groups
-  has_many :groups, throgh: :events_groups
+  has_and_belongs_to_many :groups
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  has_many :events_groups
-  has_many :events, through: :events_groups
+  has_and_belongs_to_many :events
   has_many :users, through: :events
+
 end
