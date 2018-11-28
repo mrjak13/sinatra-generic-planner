73f65839ebc2e659ba91096e54631743942e7381
Justin Kenney <justinkenney13@gmail.com>
2018-11-28 22:18:41 +0000

    add create and show actions

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,7 +1,26 @@
 class GroupController < ApplicationController
 
   get '/groups' do
+    @groups = Group.all
     erb :'/groups/index'
+  end
+
+  get '/groups/new' do
+    erb :'/grpups/new'
+  end
+
+  post '/groups' do
+    if logged_in? && !Group.find_by(name: params[:name])
+      Group.create(name: params[:name])
+      redirect to '/events/new'
+    else
+      redirect to '/groups/new'
+    end
+  end
+
+  get 'groups/:id' do
+    @group = Group.find(params[:id])
+    erb :'groups/show'
   end
 
 end
