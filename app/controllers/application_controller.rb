b7575b927f4c2222b03de88b433a9037443cacfe
Justin Kenney <justinkenney13@gmail.com>
2018-11-29 20:23:54 +0000

    add helper method if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -57,4 +57,11 @@ class ApplicationController < Sinatra::Base
     User.find(session[:user_id])
   end
 
+  def redirect_if_not_logged_in
+    if !logged_in?
+      # flash[:errors] = "You must be logged in to view the page you tried to view."
+      redirect '/'
+    end
+  end
+
 end
