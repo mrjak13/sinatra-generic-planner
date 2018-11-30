728fc9894e8a726d4d326c73087ab72d887754f2
Justin Kenney <justinkenney13@gmail.com>
2018-11-30 16:33:01 +0000

    updated db with admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 todd = User.create(name: "Todd", email: "todd@todd.com", password: "password")
 archer = User.create(name: "Archer", email: "archer@archer.com", password: "password")
 bob = User.create(name: "bob", email: "bob@bob.com", password: "password")
+admin = User.create(name: "admin", email: "admin@admin.com", password: "admin")
 
 ruby = Group.create(name: "Ruby")
 sinatra = Group.create(name: "Sinatra")
